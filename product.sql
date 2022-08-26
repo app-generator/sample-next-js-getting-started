608b8d9a66ba2ec8f2ca297da3e8f7949941bd52
koladev <onaelmangabo@gmail.com>
2022-08-26 01:15:17 +0100

    ✨ add sql script to load data in supabase db

---
+++ product.sql
@@ -0,0 +1,28 @@
+insert into "Product"
+  (name, price, image)
+values
+  (
+    'Syrup - Chocolate',
+    1,
+    'https://iambaker.net/wp-content/uploads/2020/05/syrup-fb.jpg'
+  ),
+  (
+    'Cookies Oatmeal Raisin',
+    5,
+    'https://i0.wp.com/www.livewellbakeoften.com/wp-content/uploads/2020/08/Oatmeal-Raisin-Cookies-9.jpg'
+  ),
+  (
+    'Longos - Grilled Chicken With',
+    10,
+    'https://i0.wp.com/www.themodernnonna.com/wp-content/uploads/2022/04/Easiest-2-Ingredient-Baked-Chicken.jpeg'
+  ),
+  (
+    'Dried Figs',
+    4,
+    'https://www.bottegadicalabria.com/media/magefan_blog/Fichi_secchi_1200X800.jpg'
+  ),
+  (
+    'Stock - Veal, White',
+    40,
+    'https://images.ctfassets.net/3s5io6mnxfqz/VEXn1Oiog0DgbUNU79cja/7016c63c031ee03abfb57548c5c10af4/AdobeStock_214296869.jpeg'
+  );
